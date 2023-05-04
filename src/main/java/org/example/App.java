5f67d4ee16726436dc1b272ea182b62348b1cf12
AlanMSR <124849751+AlanMSR@users.noreply.github.com>
2023-05-03 17:21:32 -0700

    First commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private String nombre;
+
+    public App(String nombre) {
+
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
